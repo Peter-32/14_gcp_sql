c05ee8689286ae6b5927b290d41d11b78e76a2fd
Peter-32 <peter@impactradius.com>
2020-12-09 23:19:36 -0800

    a

---
+++ connection_config.py
@@ -0,0 +1,11 @@
+password = 'a'
+
+connection_config = {
+    'user': 'peter2',
+    'password': password,
+    'host': '35.236.113.37',
+    'database': 'back_room',
+    'ssl_ca': 'ssl/server-ca.pem',
+    'ssl_cert': 'ssl/client-cert.pem',
+    'ssl_key': 'ssl/client-key.pem',
+}
